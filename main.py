db370352f2899603e25a423d114bb4350ecc11d2
Mr. Shaertiar <103391741+shaertiar@users.noreply.github.com>
2023-11-19 14:13:59 +0300

    Update
    Add new functions

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ fps = 12.8
 
 # Переменные 
 lines_color = (255, 0, 0)
-lines_quantity = WW/20
+lines_quantity = WW//20
+lines_width = 1
 is_print_text = False
+is_random_quantity = True
+is_show_grid = False
 
 # Создание шрифта
 font = pg.font.SysFont('Arial', WH//100)
@@ -42,6 +45,7 @@ while is_game:
                     fps /= 2
                 else:
                     fps = 0.1
+
             elif event.key == pg.K_RIGHT:
                 lines_quantity += 5
             elif event.key == pg.K_LEFT:
@@ -49,41 +53,63 @@ while is_game:
                     lines_quantity -= 5
                 else:
                     lines_quantity = 5
-            elif event.key == 96:
+
+            elif event.key == pg.K_EQUALS:
+                lines_width += 1
+            elif event.key == pg.K_MINUS:
+                if lines_width >= 2:
+                    lines_width -= 1
+
+            elif event.key == 96: # key 96 = `
                 is_print_text = not is_print_text
+
+            elif event.key == pg.K_q:
+                is_random_quantity = not is_random_quantity
+
+            elif event.key == pg.K_g:
+                is_show_grid = not is_show_grid
 
     # Создание переменной с линиями
     lines = []
 
     # Добавление линий в переменную линий
-    for i in range(random.randint(lines_quantity-4, lines_quantity)):
-        lines.append(random.randint(5, 95))
+    if is_random_quantity:
+        for i in range(random.randint(lines_quantity-4, lines_quantity)):
+            lines.append(random.randint(50, 950))
+    else: 
+        for i in range(lines_quantity):
+            lines.append(random.randint(50, 950))
 
     # Отрисовка линий
     for i in range(len(lines)):
+        if is_show_grid:
+            pg.draw.line(window, (3, 3, 50), (i*WW/len(lines), 0), (i*WW/len(lines), WH))
+            pg.draw.line(window, (3, 50, 3), (0, WH - lines[i]*WH/1000), (WW, WH - lines[i]*WH/1000))
+            if is_print_text:
+                window.blit(font.render(str(round(i*WW/len(lines), 1)), False, (6, 6, 100)), (i*WW/len(lines), 0))
+                window.blit(font.render(str(round(WH - lines[i]*WH/1000, 1)), False, (6, 100, 6)), (0, WH - lines[i]*WH/1000))
+
         try:
-            pg.draw.line(window, (255, 0, 0), 
-                (
-                    i*WW/len(lines), 
-                    WH - lines[i]*WH/100), 
-                (
-                    (i+1)*WW/len(lines), 
-                    WH - lines[i+1]*WH/100), 10)
+            pg.draw.line(window, (255, 0, 0), (i*WW/len(lines), WH - lines[i]*WH/1000), ((i+1)*WW/len(lines), WH - lines[i+1]*WH/1000), lines_width)
         except: 
-            pg.draw.line(window, (255, 0, 0), 
-                (
-                    i*WW/len(lines), 
-                    WH - lines[i]*WH/100), 
-                (
-                    (i+1)*WW/len(lines), 
-                    WH - lines[0]*WH/100))
+            pg.draw.line(window, (255, 0, 0), (i*WW/len(lines), WH - lines[i]*WH/1000), ((i+1)*WW/len(lines), WH - lines[0]*WH/1000), lines_width)
 
     # Отображение текста
     if is_print_text:
-        text = font.render(f'FPS: {float(fps)}', False, (255, 255, 255))
+        text = font.render(f'FPS: {float(fps)}/{round(clock.get_fps(), 1)} [↑][↓]', False, (255, 255, 255))
         window.blit(text, (WW-text.get_width()-10, 0))
-        text = font.render(f'Lines: {int(lines_quantity-4)}:{int(lines_quantity)}', False, (255, 255, 255))
-        window.blit(text, (WW-text.get_width()-10, WH//100))
+        if is_random_quantity:
+            text = font.render(f'Lines quantity: {int(lines_quantity-4)}:{int(lines_quantity)} [←][→]', False, (255, 255, 255))
+            window.blit(text, (WW-text.get_width()-10, WH//100))
+        else:
+            text = font.render(f'Lines quantity: {int(lines_quantity)} [←][→]', False, (255, 255, 255))
+            window.blit(text, (WW-text.get_width()-10, WH//100))
+        text = font.render(f'Linew width: {lines_width} [-][=]', False, (255, 255, 255))
+        window.blit(text, (WW-text.get_width()-10, WH//50))
+        text = font.render(f'Random lines quantity: {is_random_quantity} [q]', False, (255, 255, 255))
+        window.blit(text, (WW-text.get_width()-10, WH//33.333))
+        text = font.render(f'Show grid: {is_show_grid} [g]', False, (255, 255, 255))
+        window.blit(text, (WW-text.get_width()-10, WH//25))
 
 
     # Обновление экрана
